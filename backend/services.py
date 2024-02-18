8f16c77a40709383563cfca2525a838c48146463
Jay Sharma <jay.sharma@ama-assn.org>
2024-02-17 22:24:28 -0600

    code to search local repo or pull from remote S3 bucket

--- backend/services.py
+++ backend/services.py
@@ -1,19 +1,22 @@
 from typing import List
-import os as _os
-import random as _random
+import os as os
+import random as random
+
+import boto3
+
+def _get_image_filenames(directory_name: str, s3: boto3) -> List[str]:
+    return random.choice([images['key'] for images in s3.list_objects_v2(Bucket=os.environ['S3-BUCKET'], prefix = directory_name)['Contents']])
 
 
-def _get_image_filenames(directory_name: str) -> List[str]:
-    return _os.listdir(directory_name)
-
-
-def select_random_image(directory_name: str) -> str:
-    images = _get_image_filenames(f"data/{directory_name}")
-    random_image = _random.choice(images)
+def select_random_image(directory_name: str, s3: boto3) -> str:
+    images = _get_image_filename(directory_name, s3)
     path = f"data/{directory_name}/{random_image}"
     return path
 
+def _get_buckets(s3:boto3) -> list[str]:
+    return  [folders['Key'] for folders in s3.list_objects_v2(Bucket = os.environ['S3-BUCKET'])['Contents']]
+
 def select_image() -> str:
-    subreddit_list = ["memes","wholesomememes","2meirl4meirl","dankmemes","eyebleach","funny","happy","meirl","OneOrangeBrainCell"]
-    random_subreddit = _random.choice(subreddit_list)
-    return select_random_image(random_subreddit)
+    s3 = boto3.client('s3')
+    random_subreddit = random.choice(_get_buckets(s3))
+    return select_random_image(random_subreddit, s3)
